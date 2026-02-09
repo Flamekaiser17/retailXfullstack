e5aa43a9102fccab2ebed58251fc9b96e1b277d8
ShivanshRaj <rajputsinghshiv17@gmail.com>
2026-02-09 22:37:48 +0530

    Fix STATICFILES_DIRS configuration for Render deployment

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -210,8 +210,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "media")]
-
 # WhiteNoise configuration for production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
